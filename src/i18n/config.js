701bc1b6122f73843d3c173f211d1df0db932316
Ruslan Polozkov <rpolozkov@xbsoftware.by>
2020-10-13 11:55:17 +0300

    fix

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,18 +1,6 @@
 import i18next from "i18next"
-// import LanguageDetector from 'i18next-browser-languagedetector'
-
-// i18next.on('languageChanged', function (lng) {
-//   // if the language we switched to is the default language we need to remove the /en from URL
-//   if (lng === i18next.options.fallbackLng[0]) {
-//     if (window.location.pathname.includes('/' + i18next.options.fallbackLng[0])) {
-//       const newUrl = window.location.pathname.replace('/' + i18next.options.fallbackLng[0], '')
-//       window.location.replace(newUrl)
-//     }
-//   }
-// })
 
 i18next
-  // .use(LanguageDetector)
   .init({
   whitelist: ['en', 'ru'],
   fallbackLng: ['en'],
