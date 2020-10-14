32dfebf79702a6d873e33c376c6a523773ab3986
Ruslan Polozkov <rpolozkov@xbsoftware.by>
2020-10-14 09:25:20 +0300

    fixes

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Link } from "gatsby"
+import { graphql, Link, useStaticQuery } from "gatsby"
 import { useTranslation } from "react-i18next"
 import BackgroundImage from "gatsby-background-image"
 
@@ -10,26 +10,38 @@ import gifTravolta from "images/dominoes/second-pages/travolta-1.gif"
 
 const NotFoundPage = ({ path, children }) => {
   const { t } = useTranslation()
-
+  const data = useStaticQuery(graphql`
+    query {
+      desktop: file(relativePath: { eq: "dominoes/main-bg.jpg" }) {
+        childImageSharp {
+          fluid(quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <SEO title="Domino game" />
       <div className="wrapper">
-        <article className={"main " + 'error'}>
-          <div className="global-container">
-            <div className="main_content">
-              <div className="content_title">
-                <h2>{t(`404.content_title`)}</h2>
-                <Link to={'/'}>
-                  {t(`404.content_link`)}
-                </Link>
-              </div>
-              <div className="content_img">
-                <img src={gifTravolta} alt="Dominoes" />
+        <BackgroundImage fluid={data.desktop.childImageSharp.fluid}>
+          <article className={"main " + 'error'}>
+            <div className="global-container">
+              <div className="main_content">
+                <div className="content_title">
+                  <h2>{t(`404.content_title`)}</h2>
+                  <Link to={'/'}>
+                    {t(`404.content_link`)}
+                  </Link>
+                </div>
+                <div className="content_img">
+                  <img src={gifTravolta} alt="Dominoes" />
+                </div>
               </div>
             </div>
-          </div>
-        </article>
+          </article>
+        </BackgroundImage>
       </div>
     </Layout>
   )
