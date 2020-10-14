0c3cb1b76032ff1e983a40c203e6b23f6b285b35
Ruslan Polozkov <rpolozkov@xbsoftware.by>
2020-10-14 09:49:21 +0300

    fixes

--- src/pages/404.js
+++ src/pages/404.js
@@ -30,9 +30,9 @@ const NotFoundPage = ({ path, children }) => {
             <div className="global-container">
               <div className="main_content">
                 <div className="content_title">
-                  <h2>{t(`error.content_title`)}</h2>
+                  <h2>{t("error.content_title")}</h2>
                   <Link to={'/'}>
-                    {t(`error.content_link`)}
+                    {t("error.content_link")}
                   </Link>
                 </div>
                 <div className="content_img">
