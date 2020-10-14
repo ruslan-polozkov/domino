17f7b49ff1a74ce61271e21bba00f6dda89eb274
Ruslan Polozkov <rpolozkov@xbsoftware.by>
2020-10-14 12:11:38 +0300

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,25 @@
 import React from "react"
-
+import { BrowserRouter } from 'react-router-dom';
 import Layout from "../components/layout"
 import Wrapper from "../components/Wrapper"
 import SEO from "../components/seo"
 import { useTranslation } from "react-i18next"
 import { useStaticQuery, graphql } from "gatsby"
 import i18n from "./../i18n/config";
+import AppBanner from 'smart-app-banner-react';
 
 const IndexPage = ({ path, children }) => {
   const { t } = useTranslation()
+  const appInfo = {
+    imageUrl:
+      'https://lh3.googleusercontent.com/ccWDU4A7fX1R24v-vvT480ySh26AYp97g1VrIB_FIdjRcuQB2JP2WdY7h_wVVAeSpg=s360-rw',
+    name: 'Dominoes',
+    publisher: 'Dominoes',
+    infoText: 'Dominoes best game',
+    linkButtonText: 'VIEW',
+    linkUrl:
+      'https://play.google.com/store/apps/details?id=loppipoppi.dominoes&hl=en',
+  };
   const data = useStaticQuery(graphql`
     query {
       desktop: file(relativePath: { eq: "dominoes/main-bg.jpg" }) {
@@ -23,10 +34,23 @@ const IndexPage = ({ path, children }) => {
   data.path = path
 
   return (
-    <Layout>
-      <SEO title="Home" />
-      <Wrapper {...data} />
-    </Layout>
+    <BrowserRouter>
+      <AppBanner
+        appInfo={appInfo}
+        position="bottom"
+        onClose={() => {
+          console.log('onClose called');
+        }}
+        hidden={false}
+        style={{
+          zIndex: 10
+        }}
+      />
+      <Layout path={path}>
+        <SEO title="Home" />
+        <Wrapper {...data} />
+      </Layout>
+    </BrowserRouter>
   )
 }
 
